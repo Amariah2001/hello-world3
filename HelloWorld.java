096b3e8c076de0b7d35234f780a9b06defa9a8b5
Amariah Robinson <arobin95@email.cpcc.edu>
2024-02-09 09:50:38 -0500

    Create HelloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+// Amariah Robinson
+// Github Repositories 
+// 02/09/2024
+// CSC 251:N801
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
